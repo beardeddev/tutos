c3da59f1011e0a15062ad3919c6d52cd67ebef77
beardeddev <beardeddev@gmail.com>
2020-03-09 13:20:17 +0200

    add ng-content to child component

--- ToDoListAppClient/src/app/app.component.ts
+++ ToDoListAppClient/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { ChildComponent }   from './child.component';
-
       
 @Component({
     selector: 'my-app',
-    template: `<child-comp></child-comp>
-                <p>Привет {{name}}</p>`,
+    template: `<child-comp><h2>Добро пожаловать {{name}}!</h2></child-comp>`,
     styles: [`h2, p {color:#333;}`]
 })
 export class AppComponent { 
-    name = 'Петр';
+    name = 'Tom';
 }
