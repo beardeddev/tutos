7ba3281b96246e4fcb39716b5e126c204dc302bb
beardeddev <beardeddev@gmail.com>
2020-03-09 20:07:47 +0200

    add attribute deirective

--- ToDoListAppClient/src/app/app.component.ts
+++ ToDoListAppClient/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component} from '@angular/core';
-      
+ 
 @Component({
-    selector: 'my-app',
-    template: `<child-comp [name]="name"></child-comp>
-                <input type="text" [(ngModel)]="name" />`
+    selector: 'my-app',
+    template: `<div>
+                  <p bold>Hello Angular 9</p>
+                  <p>Angular 9 представляет модульную архитектуру приложения</p>
+               </div>`
 })
-export class AppComponent{ 
-     name:string="Tom";
-}
+export class AppComponent {}
