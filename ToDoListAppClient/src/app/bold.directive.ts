1d9a8b9a9c1b085105979904db041f983a7ba9bb
beardeddev <beardeddev@gmail.com>
2020-03-11 11:48:04 +0200

    add HostListener to bold directive

--- ToDoListAppClient/src/app/bold.directive.ts
+++ ToDoListAppClient/src/app/bold.directive.ts
@@ -1,12 +1,24 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
- 
+import {Directive, ElementRef, Renderer2, HostListener} from '@angular/core';
+ 
 @Directive({
-    selector: '[bold]'
+    selector: '[bold]'
 })
 export class BoldDirective{
-     
-    constructor(private elementRef: ElementRef, private renderer: Renderer2){
-         
-        this.renderer.setStyle(this.elementRef.nativeElement, "font-weight", "bold");
-    }
+     
+    constructor(private element: ElementRef, private renderer: Renderer2){
+         
+        this.renderer.setStyle(this.element.nativeElement, "cursor", "pointer");
+    }
+     
+    @HostListener("mouseenter") onMouseEnter() {
+        this.setFontWeight("bold");
+    }
+ 
+    @HostListener("mouseleave") onMouseLeave() {
+        this.setFontWeight("normal");
+    }
+ 
+    private setFontWeight(val: string) {
+        this.renderer.setStyle(this.element.nativeElement, "font-weight", val);
+    }
 }
